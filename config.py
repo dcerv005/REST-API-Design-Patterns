17983b34b80589fee59da8a1b2d3223aa608eae2
dcerv005 <dacervantes12@gmail.com>
2024-08-01 23:57:35 -0700

    first commit

---
+++ config.py
@@ -0,0 +1,4 @@
+class DevelopmentConfigAssignment:
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:******@localhost/module13_lesson1'
+    CACHE_TYPE= 'SimpleCache'
+    DEBUG = True
---
+++ controllers/productController.py
@@ -0,0 +1,25 @@
+from flask import jsonify, request
+from models.schemas.productSchema import product_schema, products_schema
+from marshmallow import ValidationError
+from services import productService
+from caching import cache
+
+
+def save():
+    try:
+
+        #Validate and deserialize input
+        product_data = product_schema.load(request.json)
+    except ValidationError as err:
+        return jsonify(err.messages), 400
+    
+    try:
+        product_save = productService.save(product_data)
+        return product_schema.jsonify(product_save), 201
+    except ValueError as e:
+        return jsonify({"error": str(e)}), 400
+    
+@cache.cached(timeout=60)    
+def find_all():
+    products = productService.find_all()
+    return products_schema.jsonify(products), 200
